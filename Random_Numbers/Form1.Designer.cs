37025bb3527bc3764e55cfded1c00d7ed5974a61
Zachary Betters <zachbet97@outlook.com>
2016-09-07 10:44:12 -0400

    Ice cream flavor demo

--- Random_Numbers/Form1.Designer.cs
+++ Random_Numbers/Form1.Designer.cs
@@ -28,12 +28,80 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbxFlavors = new System.Windows.Forms.ListBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lblFlavor = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Lucida Calligraphy", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.label1.Location = new System.Drawing.Point(8, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(187, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ice Cream Flavors:";
+            // 
+            // lbxFlavors
+            // 
+            this.lbxFlavors.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbxFlavors.FormattingEnabled = true;
+            this.lbxFlavors.ItemHeight = 18;
+            this.lbxFlavors.Location = new System.Drawing.Point(12, 37);
+            this.lbxFlavors.Name = "lbxFlavors";
+            this.lbxFlavors.Size = new System.Drawing.Size(260, 130);
+            this.lbxFlavors.TabIndex = 1;
+            this.lbxFlavors.SelectedIndexChanged += new System.EventHandler(this.lbxFlavors_SelectedIndexChanged);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(12, 232);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(81, 50);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // lblFlavor
+            // 
+            this.lblFlavor.AutoSize = true;
+            this.lblFlavor.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblFlavor.Font = new System.Drawing.Font("Monotype Corsiva", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFlavor.ForeColor = System.Drawing.Color.Fuchsia;
+            this.lblFlavor.Location = new System.Drawing.Point(12, 187);
+            this.lblFlavor.Name = "lblFlavor";
+            this.lblFlavor.Size = new System.Drawing.Size(64, 24);
+            this.lblFlavor.TabIndex = 3;
+            this.lblFlavor.Text = "Flavor";
+            this.lblFlavor.Click += new System.EventHandler(this.lblFlavor_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(284, 294);
+            this.Controls.Add(this.lblFlavor);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.lbxFlavors);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Ice Cream";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox lbxFlavors;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lblFlavor;
     }
 }
 
--- Random_Numbers/Form1.cs
+++ Random_Numbers/Form1.cs
@@ -1,20 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Random_Numbers
-{
-    public partial class Form1 : Form
-    {
-        public Form1()
-        {
-            InitializeComponent();
+namespace Random_Numbers {
+    public partial class Form1 : Form {
+        public Form1() { InitializeComponent(); }
+        private void btnExit_Click(object sender, EventArgs e) {
+            Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            lbxFlavors.Items.Add("Chocolate");
+            lbxFlavors.Items.Add("Vanilla");
+            lbxFlavors.Items.Add("Cookie Dough");
+            lbxFlavors.Items.Add("Strawberry");
+            lbxFlavors.Items.Add("Monster Mash");
+            lbxFlavors.Items.Add("Birthday Cake");
+            lbxFlavors.Items.Add("Rockin' Poppin' Cotton Candy");
+            lbxFlavors.Items.Add("Hunka Chunka PB Fudge");
+            lbxFlavors.Items.Add("Jim Dandy");
+        }
+
+        private void lblFlavor_Click(object sender, EventArgs e) { }
+
+        private void lbxFlavors_SelectedIndexChanged(object sender, EventArgs e) {
+            lblFlavor.Text = lbxFlavors.SelectedItem.ToString();
         }
     }
 }
