b26c14ef92b48300db872c3dcc5bb78ee812721d
Zachary Betters <zachbet97@outlook.com>
2016-09-12 09:26:18 -0400

    Lists Ice Cream flavors

--- Random_Numbers/Form1.cs
+++ Random_Numbers/Form1.cs
@@ -28,7 +28,6 @@ namespace Random_Numbers {
         }
 
         private void lblFlavor_Click(object sender, EventArgs e) { }
-
         private void lbxFlavors_SelectedIndexChanged(object sender, EventArgs e) {
             lblFlavor.Text = lbxFlavors.SelectedItem.ToString();
         }
